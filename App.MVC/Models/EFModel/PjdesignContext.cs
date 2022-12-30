1b11cd886f8667c840872ee20a496ba93d9caf5a
fee8ack21 <fee8ack21@gmail.com>
2022-12-30 20:18:56 +0800

    1.connection string 補充 2.input 瀏覽器支援調整 3.footer text 瀏覽器支援調整

--- App.MVC/Models/EFModel/PjdesignContext.cs
+++ App.MVC/Models/EFModel/PjdesignContext.cs
@@ -44,11 +44,6 @@ namespace App.MVC.Models.EFModel
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=PJDesign;Trusted_Connection=True;Integrated Security=True");
-            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
